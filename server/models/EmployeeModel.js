8c7d0d3ad39232189c994f901eaf7668ed7c6bf0
prince-arun <prince.skillsafari@gmail.com>
2023-11-18 02:30:51 +0530

    Employee schema updated

--- server/models/EmployeeModel.js
+++ server/models/EmployeeModel.js
@@ -5,21 +5,39 @@ const employeeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  age: {
-    type: Number,
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female", "other"],
-    required: true,
-  },
   dob: {
     type: Date,
     required: true,
   },
+  gender: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  industry: {
+    type: String,
+    required: true,
+  },
+  workingHours: {
+    type: String,
+    required: true,
+  },
   country: {
     type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+  isAgree: {
+    type: Boolean,
     required: true,
   },
 });
